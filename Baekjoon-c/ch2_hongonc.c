ce6395fbc2d9a688658f47cbdea32c70aa22c6db
yangwoo10 <hansolo1110@naver.com>
2025-01-27 17:10:42 +0900

    챕터 2 마무리
    정수, 실수 등 사과 표현

---
+++ Baekjoon-c/ch2_hongonc.c
@@ -0,0 +1,49 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("%d\n", 12);
+//	printf("%d\n", 014);
+//	printf("%d\n", 0xc);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("%.1lf\n", 1e6);
+//	printf("%.7lf\n", 3.14e-5);
+//	printf("%le\n", 0.0000314);
+//	printf("%.2le\n", 0.0000314);
+//}
+
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	printf("%c\n", 'A');
+//	printf("%s\n", "A");
+//	printf("%c는 %s입니다.\n", '1', "first");
+//
+//	return 0;
+//}
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("학번 : %d\n", 2020);
+	printf("이름 : %s\n", "양우");
+	printf("학점 : %c\n", 'A');
+
+	return 0;
+}
+
+
+
+
+
+
